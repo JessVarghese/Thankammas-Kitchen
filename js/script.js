502178175958bb9076cab652b3912234ecee7848
Jess Varghese <jessvarghese@gmail.com>
2022-03-29 13:03:54 -0400

    added email signup modal

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
 
-//Contact Us Modal
+//Signup  Modal
 
-// Get the modal
-var modal = document.getElementById('myModal');
+var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+
+
